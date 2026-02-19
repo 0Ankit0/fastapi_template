fe58b2b869b46a4e109c07bd4da6d8d7eabc439c
0ankit0 <ankitpaudel.1373@gmail.com>
2026-02-19 09:14:37 +0545

    added required packages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from fastapi-template!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
