86891273da668a75c19c46b6bd34ee8f942e68fe
0ankit0 <ankitpaudel.1373@gmail.com>
2026-06-18 06:50:14 +0545

    feat: added script to automatically add the access_token to the environment variable on login and added example for all the organization related api endpoints

--- src/apps/organizations/api/v1/__init__.py
+++ src/apps/organizations/api/v1/__init__.py
@@ -3,7 +3,7 @@ from .organization import router as organizations_router
 from fastapi import APIRouter
 
 def get_all_organization_routers() -> APIRouter:
-    router = APIRouter(prefix="/v1")
+    router = APIRouter(prefix="/api/v1")
     router.include_router(organizations_router)
     router.include_router(organization_members_router)
     return router
