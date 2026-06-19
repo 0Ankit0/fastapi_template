5d76eb28b3c45f90878b4ec34221bb3f6d7169f1
0ankit0 <ankitpaudel.1373@gmail.com>
2026-06-19 07:56:01 +0545

    fix: used organization slug instead of id for the policyservice for easier retrieval

--- src/apps/iam/services/policy_service.py
+++ src/apps/iam/services/policy_service.py
@@ -1,5 +1,7 @@
 # app/services/policy_service.py
 
+from src.core.eums import RBACRole
+
 from ..casbin import enforcer
 from src.apps.iam.models import User
 
@@ -13,13 +15,13 @@ class PolicyService:
     @staticmethod
     def add_permission(
         role: str,
-        org: str,
+        org_slug: str,
         module: str,
         action: str,
     ) -> bool:
         return enforcer.add_policy(
             role,
-            org,
+            org_slug,
             module,
             action,
         )
@@ -27,23 +29,23 @@ class PolicyService:
     @staticmethod
     def remove_permission(
         role: str,
-        org: str,
+        org_slug: str,
         module: str,
         action: str,
     ) -> bool:
         return enforcer.remove_policy(
             role,
-            org,
+            org_slug,
             module,
             action,
         )
 
     @staticmethod
-    def get_permissions(role: str,org: str):
+    def get_permissions(role: str,org_slug: str):
         return enforcer.get_filtered_policy(
             0,
             role,
-            org
+            org_slug
         )
 
     # -------------------------
@@ -52,37 +54,52 @@ class PolicyService:
 
     @staticmethod
     def assign_role(
-        user: User,
+        user_id: int,
         role: str,
-        org: str,
+        org_slug: str,
     ) -> bool:
         # TODO: Validate that the user belongs to the organization and that the role exists
         return enforcer.add_grouping_policy(
-            user.id,
+            user_id,
             role,
-            org,
+            org_slug,
         )
 
     @staticmethod
     def revoke_role(
-        user: User,
+        user_id: int,
         role: str,
-        org: str,
+        org_slug: str,
     ) -> bool:
         return enforcer.remove_grouping_policy(
-            user.id,
+            user_id,
             role,
-            org,
+            org_slug,
         )
 
     @staticmethod
     def get_user_roles(
-        user: User,
-        org: str,
+        user_id: int,
+        org_slug: str,
     ):
         return enforcer.get_roles_for_user_in_domain(
-            user.id,
-            org,
+            user_id,
+            org_slug,
+        )
+    
+    @staticmethod
+    def remove_user_with_roles(
+        user_id: int,
+        org_slug: str,
+    ):
+        roles = enforcer.get_roles_for_user_in_domain(
+            user_id,
+            org_slug,
+        )
+        return enforcer.delete_roles_for_user_in_domain(
+            user_id,
+            roles,
+            org_slug,
         )
 
     # -------------------------
@@ -93,24 +110,24 @@ class PolicyService:
     def inherit_role(
         role: str,
         parent_role: str,
-        org: str,
+        org_slug: str,
     ) -> bool:
         return enforcer.add_grouping_policy(
             role,
             parent_role,
-            org,
+            org_slug,
         )
     
     @staticmethod
     def remove_role_inheritance(
         role: str,
         parent_role: str,
-        org: str,
+        org_slug: str,
     ) -> bool:
         return enforcer.remove_grouping_policy(
             role,
             parent_role,
-            org,
+            org_slug,
         )
 
     # -------------------------
@@ -120,7 +137,7 @@ class PolicyService:
     @staticmethod
     def has_permission(
         user: User,
-        org: str,
+        org_slug: str,
         module: str,
         action: str,
     ) -> bool:
@@ -130,7 +147,34 @@ class PolicyService:
         
         return enforcer.enforce(
             user.id,
-            org,
+            org_slug,
             module,
             action,
         )
+    
+    # ==========================
+    # Organization Roles
+    # ==========================
+
+    @staticmethod
+    @staticmethod
+    def get_org_roles(org_slug: str) -> dict[int, list[RBACRole]]:
+        """
+        Returns:
+        {
+            1: ["admin"],
+            2: ["manager", "editor"]
+        }
+        """
+
+        policies = enforcer.get_filtered_grouping_policy(
+            2,  # v2 = org/domain
+            org_slug,
+        )
+
+        role_map: dict[int, list[RBACRole]] = {}
+
+        for user_id, role, _org in policies:
+            role_map.setdefault(user_id, []).append(role)
+
+        return role_map
