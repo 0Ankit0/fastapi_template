4728469c18a2c48b1cba35e61766a69e1357b4a6
0ankit0 <ankitpaudel.1373@gmail.com>
2026-07-08 06:39:15 +0545

    feat: updated the get_al_routers method to send back all the newly added endpoints

--- src/apps/__init__.py
+++ src/apps/__init__.py
@@ -4,16 +4,20 @@ def load_all_models() -> None:
     """Import all models to ensure they are registered with SQLAlchemy metadata."""
     from .iam.models import User, UserProfile, TokenTracking, LoginAttempt, UsedToken
     from .organizations.models import Organization
+    from .notification.models import Notification, NotificationPreference
 
 def get_all_routers() -> APIRouter:
     """Return a list of all API routers from the apps."""
     from .iam.api.v1 import get_all_iam_routers
+    from .notification.api.v1 import get_all_notification_routers
     from .organizations.api.v1 import get_all_organization_routers
-    from .websockets.api import router as websocket_router
+    from .realtime.api import sse_router, websocket_router
 
     router = APIRouter()
     router.include_router(get_all_iam_routers())
+    router.include_router(get_all_notification_routers())
     router.include_router(get_all_organization_routers())
     router.include_router(websocket_router)
+    router.include_router(sse_router)
 
     return router
